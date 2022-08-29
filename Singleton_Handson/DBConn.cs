1b646317feb887e822bece76a617cf1c44ebec0b
Kumari <2064588@cognizant.com>
2022-08-29 20:57:40 +0530

    first commit

---
+++ Singleton_Handson/DBConn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Handson
+{
+    internal class DBConn
+    {
+        private static DBConn instance = new DBConn();
+        private DBConn() { }
+        public static DBConn GetInstance()
+        {
+            return instance;
+        }
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+    public class DBConnX
+    {
+        public static void Main(String[] args)
+        {
+
+
+            DBConn object1 = DBConn.GetInstance();
+
+            object1.ShowMessage();
+
+        }
+    }
+}
